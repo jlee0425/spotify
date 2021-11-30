f3ab1f64296d77be9c36a325c5f6c82587b93227
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-11-30 23:03:13 +0900

    refactor css component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,46 +12,46 @@ const Sidebar = () => {
 	return (
 		<div className="text-gray-500 p-5 text-sm border-r border-gray-900">
 			<div className="space-y-4">
-				<button className="flex items-center space-x-2 hover:text-white">
+				<button className="main-btn">
 					<HomeIcon className="h-5 w-5" />
 					<p>Home</p>
 				</button>
-				<button className="flex items-center space-x-2 hover:text-white">
+				<button className="main-btn">
 					<SearchIcon className="h-5 w-5" />
 					<p>Search</p>
 				</button>
-				<button className="flex items-center space-x-2 hover:text-white">
+				<button className="main-btn">
 					<LibraryIcon className="h-5 w-5" />
 					<p>Your Library</p>
 				</button>
-				<hr className="border-t-[0.1px] border-gray-900" />
-				<button className="flex items-center space-x-2 hover:text-white">
+				<hr className="divider" />
+				<button className="main-btn">
 					<PlusCircleIcon className="h-5 w-5" />
 					<p>Create Playlist</p>
 				</button>
-				<button className="flex items-center space-x-2 hover:text-white">
+				<button className="main-btn">
 					<HeartIcon className="h-5 w-5" />
 					<p>Liked Songs</p>
 				</button>
-				<button className="flex items-center space-x-2 hover:text-white">
+				<button className="main-btn">
 					<RssIcon className="h-5 w-5" />
 					<p>Your Episodes</p>
 				</button>
-				<hr className="border-t-[0.1px] border-gray-900" />
+				<hr className="divider" />
 				{/* Playlists */}
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
+				<p className="playlist">Playlist name...</p>
 			</div>
 		</div>
 	);
