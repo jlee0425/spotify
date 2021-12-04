3ed1a679f318035e1bbc7f1fcac1b3c3f18ab6ce
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-12-04 17:25:30 +0900

    hide scrollbar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ import React from 'react';
 
 const Sidebar = () => {
 	return (
-		<div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+		<div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
 			<div className="space-y-4">
 				<button className="main-btn">
 					<HomeIcon className="h-5 w-5" />
