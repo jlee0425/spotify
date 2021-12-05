e23a17149eae30049b691e0b713bd2315c24090b
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-12-05 12:05:43 +0900

    merged spotify into custom hook

--- hooks/useSpotify.ts
+++ hooks/useSpotify.ts
@@ -1,19 +1,50 @@
-import spotifyAPI from 'lib/spotify';
 import { signIn, useSession } from 'next-auth/react';
 import { useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const scopes = [
+	'user-read-email',
+	'playlist-read-private',
+	'playlist-read-collaborative',
+	'streaming',
+	'user-read-private',
+	'user-library-read',
+	'user-top-read',
+	'user-read-playback-state',
+	'user-modify-playback-state',
+	'user-read-currently-playing',
+	'user-read-recently-played',
+	'user-follow-read',
+].join(',');
+
+const params = {
+	scope: scopes,
+};
+
+const queryParam = new URLSearchParams(params);
+export const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParam.toString()}`;
+const spotifyAPI = new SpotifyWebApi({
+	clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+	clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRENT,
+});
+
+export default spotifyAPI;
 
 export const useSpotify = () => {
 	const { data: session, status } = useSession();
 
 	useEffect(() => {
 		if (session) {
-			if (session.error === 'RefreshAccessTokenError') {
+			if (
+				status === 'unauthenticated' ||
+				session.error === 'RefreshAccessTokenError'
+			) {
 				signIn();
 			}
 
 			spotifyAPI.setAccessToken(session!.user!.accessToken);
 		}
-	}, [session]);
+	}, [session, status]);
 
 	return spotifyAPI;
 };
